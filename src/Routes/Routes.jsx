58e5564f6ed216f4ce07810204982b8ec25e584b
Dola Barai <dola43dc@gmail.com>
2023-04-27 12:46:02 +0600

    news added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home/Home";
 import Category from '../Pages/Home/Category/Category';
 import NewsLayout from '../Layouts/NewsLayout';
+import News from '../Pages/News/News/News';
 
 
 const router = createBrowserRouter([
@@ -16,7 +17,8 @@ const router = createBrowserRouter([
         },
         {
           path: "/category/:id",
-          element: <Category></Category> 
+          element: <Category></Category>,
+          loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
         }
       ]
     },
@@ -25,7 +27,9 @@ const router = createBrowserRouter([
       element: <NewsLayout></NewsLayout>,
       children: [
         {
-          
+          path: "/news/:id",
+          element: <News></News>,
+          loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
         }
       ]
       
